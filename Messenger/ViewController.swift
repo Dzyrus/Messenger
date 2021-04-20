a52dbc2d5ebe956463433a485b1e70d358c03a35
OUT-Mishchenko1-DY <DYuMishchenko@sberbank.ru>
2021-04-20 10:08:25 +0300

    init

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by OUT-Mishchenko1-DY on 20.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
